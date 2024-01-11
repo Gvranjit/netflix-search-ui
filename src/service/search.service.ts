d230948841a56590b0ad05d90cc96b362f407e33
Gaurav Ranjit <gvranjit@gmail.com>
2024-01-11 17:11:34 -0500

    Refactor constants and types

--- src/service/search.service.ts
+++ src/service/search.service.ts
@@ -2,15 +2,17 @@
 
 import axios from "axios";
 import { useState } from "react";
-import { Api, SearchResponse } from "./service.constants";
+import { Api } from "./service.constants";
+import { SearchResponse } from "./service.model";
 
 const useSearch = () => {
+  console.log(import.meta.env.VITE_BASE_URL);
   const [results, setResults] = useState<SearchResponse>();
   async function searchTitle(query: string) {
     try {
       const { data } = await axios.request({
         method: "get",
-        baseURL: Api.BASE_URL,
+        baseURL: import.meta.env.VITE_BASE_URL || Api.BASE_URL,
         url: Api.SEARCH,
         params: { query },
       });
--- src/service/service.constants.ts
+++ src/service/service.constants.ts
@@ -2,28 +2,3 @@ export const enum Api {
   BASE_URL = "https://netflix.gauravranjit.com/api",
   SEARCH = "search",
 }
-
-export type SearchResponseResult = {
-  id: number;
-  title: string;
-  img: string;
-  vtype: string;
-  nfid: number;
-  synopsis: string;
-  avgrating: number;
-  year: number;
-  runtime: number;
-  imdbid: string;
-  poster: string;
-  imdbrating: number;
-  top250: number;
-  top250tv: number;
-  clist: string;
-  titledate: string;
-  countries: { [key in string]: string };
-};
-export type SearchResponse = {
-  results: SearchResponseResult[];
-  total: number;
-  elapse: number;
-};
